f4e23892f8b35017edc11ec5beb81d618b939522
narmijo-tj <toffee.jaguargmail.com>
2024-09-06 16:00:28 -0400

    Sin cambios sustanciales, solo se eliman espacios de lineas vacias

--- src/Cliente.js
+++ src/Cliente.js
@@ -1,34 +1,27 @@
 class Cliente {
     #nombre;
     #impuesto;
-
     constructor(nombre, impuesto) {
         this.#nombre = nombre;
         this.#impuesto = impuesto;
     }
-
     // Métodos get y set para nombre
     get nombre() {
         return this.#nombre;
     }
-
     set nombre(value) {
         this.#nombre = value;
     }
-
     // Métodos get y set para impuesto
     get impuesto() {
         return this.#impuesto;
     }
-
     set impuesto(value) {
         this.#impuesto = value;
     }
-
     // Método para calcular el impuesto
     calcularImpuesto() {
         return (this.#impuesto.montoBrutoAnual - this.#impuesto.deducciones) * 0.21;
     }
 }
-
 export default Cliente;
